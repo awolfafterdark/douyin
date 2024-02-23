9bef1e49169504d78e4d9d7238bd089699acdb2e
awolfafterdark <32263318+awolfafterdark@users.noreply.github.com>
2024-02-23 08:04:12 -0800

    Update fay.go

--- fay/impl/fay.go
+++ fay/impl/fay.go
@@ -1,147 +1,147 @@
 package impl
 
 import (
-	"encoding/json"
-	"github.com/gorilla/websocket"
-	"github.com/wwengg/douyin/fay"
-	"github.com/wwengg/douyin/proto"
-	"log"
-	"net/http"
-	"time"
+"encoding/json"
+"github.com/gorilla/websocket"
+"github.com/wwengg/douyin/fay"
+"github.com/wwengg/douyin/proto"
+"log"
+"net/http"
+"time"
 )
 
 type FayProxyServer struct {
-	// Websocket Addr
-	WsAddr string
-	//用于缓存接收过的消息ID，判断是否重复接收
-	Dictionary map[string][]int64
-	//当前Server的链接管理器
-	ConnMgr fay.ConnManager
+// Websocket Addr
+WsAddr string
+//Used to cache the received message ID and determine whether to receive it repeatedly.
+Dictionary map[string][]int64
+//The link manager of the current Server
+ConnMgr fay.ConnManager
 
-	GenID uint64
+GenID uint64
 }
 
 func NewFayProxyServer() *FayProxyServer {
-	return &FayProxyServer{
-		WsAddr:     "127.0.0.1:8888",
-		Dictionary: make(map[string][]int64),
-		ConnMgr:    NewConnManager(),
-	}
+return &FayProxyServer{
+WsAddr: "127.0.0.1:8888",
+Dictionary: make(map[string][]int64),
+ConnMgr: NewConnManager(),
+}
 }
 
-// GetConnMgr 得到链接管理
+// GetConnMgr gets the link management
 func (s *FayProxyServer) GetConnMgr() fay.ConnManager {
-	return s.ConnMgr
+return s.ConnMgr
 }
 
-// Start Websocket网络服务
+// Start Websocket network service
 func (s *FayProxyServer) StartWebsocket() {
-	//logger.ZapLog.Info("Start Websocket server", zap.String("addr", s.WsAddr))
-	log.Printf("Start Websocket server addr:%s（用于对接Fay https://github.com/TheRamU/Fay/tree/fay-sales-edition）", s.WsAddr)
-	httpServer := &http.Server{
-		Addr: s.WsAddr,
-		Handler: &WsHandler{upgrader: websocket.Upgrader{
-			HandshakeTimeout: 0,
-			ReadBufferSize:   0,
-			WriteBufferSize:  0,
-			WriteBufferPool:  nil,
-			Subprotocols:     nil,
-			Error:            nil,
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-			EnableCompression: false,
-		},
-			sv: s},
-		ReadTimeout:    time.Second * time.Duration(60),
-		WriteTimeout:   time.Second * time.Duration(60),
-		MaxHeaderBytes: 4096,
-	}
-	httpServer.ListenAndServe()
+//logger.ZapLog.Info("Start Websocket server", zap.String("addr", s.WsAddr))
+log.Printf("Start Websocket server addr:%s (used to connect to Fay https://github.com/TheRamU/Fay/tree/fay-sales-edition)", s.WsAddr)
+httpServer := &http.Server{
+Addr: s.WsAddr,
+Handler: &WsHandler{upgrader: websocket.Upgrader{
+HandshakeTimeout: 0,
+ReadBufferSize: 0,
+WriteBufferSize: 0,
+WriteBufferPool: nil,
+Subprotocols: nil,
+Error: nil,
+CheckOrigin: func(r *http.Request) bool {
+return true
+},
+EnableCompression: false,
+},
+sv: s},
+ReadTimeout: time.Second * time.Duration(60),
+WriteTimeout: time.Second * time.Duration(60),
+MaxHeaderBytes: 4096,
+}
+httpServer.ListenAndServe()
 }
 
-type WsHandler struct {
-	sv       *FayProxyServer
-	upgrader websocket.Upgrader
+typeWsHandler struct {
+sv *FayProxyServer
+upgrader websocket.Upgrader
 }
 
 func (h *WsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-	conn, err := h.upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatalf("upgrade error,err:%s", err.Error())
-		return
-	}
+if r.URL.Path != "/" {
+http.Error(w, "Not found", http.StatusNotFound)
+return
+}
+if r.Method != "GET" {
+http.Error(w, "Method not allowed", 405)
+return
+}
+conn, err := h.upgrader.Upgrade(w, r, nil)
+if err != nil {
+log.Fatalf("upgrade error,err:%s", err.Error())
+return
+}
 
-	//3.2 设置服务器最大连接控制,如果超过最大连接，那么则关闭此新的连接
-	if h.sv.ConnMgr.Len() >= 10 {
-		conn.Close()
-	}
+//3.2 Set the server's maximum connection control. If the maximum connection is exceeded, the new connection will be closed.
+if h.sv.ConnMgr.Len() >= 10 {
+conn.Close()
+}
 
-	//3.3 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
-	h.sv.GenID++
-	dealConn := NewConnection(h.sv, h.sv.GenID, NewWsProtocol(conn))
+//3.3 Business method to handle the new connection request. At this time, handler and conn should be bound.
+h.sv.GenID++
+dealConn := NewConnection(h.sv, h.sv.GenID, NewWsProtocol(conn))
 
-	h.sv.GetConnMgr().Add(dealConn)
-	//3.4 启动当前链接的处理业务
-	go dealConn.Start()
+h.sv.GetConnMgr().Add(dealConn)
+//3.4 Start the processing business of the current link
+go dealConn.Start()
 
 }
 
 func (s *FayProxyServer) DoMessage(message *proto.Message) {
-	list, ok := s.Dictionary[message.Method]
-	if !ok {
-		list = []int64{}
-	} else {
-		for _, i := range list {
-			if i == message.MsgId {
-				return
-			}
-		}
-	}
-	if len(list) > 300 {
-		list = []int64{}
-	}
-	log.Printf("method:%s,num:%d", message.Method, len(list))
-	list = append(list, message.MsgId)
-	s.Dictionary[message.Method] = list
-	switch message.Method {
-	// 用户进入
-	case "WebcastMemberMessage":
-		memberMessage := proto.MemberMessage{}
-		memberMessage.XXX_Unmarshal(message.Payload)
-		s.onMemberMessgae(memberMessage)
-		break
-	// 用户留言
-	case "WebcastChatMessage":
-		chatMessage := proto.ChatMessage{}
-		chatMessage.XXX_Unmarshal(message.Payload)
-		s.onChatMessage(chatMessage)
-		break
-	default:
-		break
-	}
+list, ok := s.Dictionary[message.Method]
+if !ok {
+list = []int64{}
+} else {
+for _, i := range list {
+if i == message.MsgId {
+return
+}
+}
+}
+if len(list) > 300 {
+list = []int64{}
+}
+log.Printf("method:%s,num:%d", message.Method, len(list))
+list = append(list, message.MsgId)
+s.Dictionary[message.Method] = list
+switch message.Method {
+// User enters
+case "WebcastMemberMessage":
+memberMessage := proto.MemberMessage{}
+memberMessage.XXX_Unmarshal(message.Payload)
+s.onMemberMessgae(memberMessage)
+break
+	// User Comments
+case "WebcastChatMessage":
+chatMessage := proto.ChatMessage{}
+chatMessage.XXX_Unmarshal(message.Payload)
+s.onChatMessage(chatMessage)
+break
+default:
+break
+}
 
 }
 
 func (s *FayProxyServer) send(pack *fay.MsgPack) {
-	data, _ := json.Marshal(pack)
-	s.GetConnMgr().SendMsgToAllConn(data)
+data, _ := json.Marshal(pack)
+s.GetConnMgr().SendMsgToAllConn(data)
 }
 
 func (s *FayProxyServer) onMemberMessgae(message proto.MemberMessage) {
-	s.send(fay.CreateMsgPack(message, fay.MsgType_JoinRoom))
-	log.Printf("%s 来了", message.User.Nickname)
+s.send(fay.CreateMsgPack(message, fay.MsgType_JoinRoom))
+log.Printf("%s is here", message.User.Nickname)
 }
 
 func (s *FayProxyServer) onChatMessage(message proto.ChatMessage) {
-	s.send(fay.CreateMsgPack(message, fay.MsgType_DanMu))
-	log.Printf("%s说: %s", message.User.Nickname, message.Content)
+s.send(fay.CreateMsgPack(message, fay.MsgType_DanMu))
+log.Printf("%s said: %s", message.User.Nickname, message.Content)
 }
