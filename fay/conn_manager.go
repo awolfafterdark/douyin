1bb2b298645b13fbf231a437b863f3a985cabbce
awolfafterdark <32263318+awolfafterdark@users.noreply.github.com>
2024-02-23 08:19:32 -0800

    Update conn_manager.go

--- fay/conn_manager.go
+++ fay/conn_manager.go
@@ -1,11 +1,11 @@
 package fay
 
 type ConnManager interface {
-	Add(conn Connection)                   //添加链接
-	Remove(conn Connection)                //删除连接
-	Get(connID uint64) (Connection, error) //利用ConnID获取链接
-	Len() int                              //获取当前连接
-	ClearConn()                            //删除并停止所有链接
-	SendMsgToAllConn(data []byte)          // 发送
+Add(conn Connection) //Add link
+Remove(conn Connection) //Delete connection
+Get(connID uint64) (Connection, error) //Use ConnID to get the link
+Len() int //Get the current connection
+ClearConn() //Delete and stop all links
+SendMsgToAllConn(data []byte) // Send
 
 }
