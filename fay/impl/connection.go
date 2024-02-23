29b267470656c2aa4d702b3a28ec58d89ff5fa82
awolfafterdark <32263318+awolfafterdark@users.noreply.github.com>
2024-02-23 08:03:35 -0800

    Update connection.go

--- fay/impl/connection.go
+++ fay/impl/connection.go
@@ -1,224 +1,224 @@
 // @Title
 // @Description
-// @Author  Wangwengang  2022/3/30 下午11:50
-// @Update  Wangwengang  2022/3/30 下午11:50
+// @Author Wangwengang 2022/3/30 11:50 pm
+// @Update Wangwengang 2022/3/30 11:50 pm
 package impl
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"github.com/wwengg/douyin/fay"
-	"io"
-	"sync"
+"context"
+"errors"
+"fmt"
+"github.com/wwengg/douyin/fay"
+"io"
+"sync"
 )
 
 type Connection struct {
-	FayProxyServer *FayProxyServer
-	//当前连接的socket TCP套接字
-	//Conn *net.TCPConn
-	//当前连接的ID 也可以称作为SessionID，ID全局唯一
-	ConnID uint64
-	//消息管理MsgID和对应处理方法的消息管理模块
-	//MsgHandler anet2.MsgHandle
-	//告知该链接已经退出/停止的channel
-	ctx    context.Context
-	cancel context.CancelFunc
-	//无缓冲管道，用于读、写两个goroutine之间的消息通信
-	msgChan chan []byte
-	//有缓冲管道，用于读、写两个goroutine之间的消息通信
-	msgBuffChan chan []byte
+FayProxyServer *FayProxyServer
+//The currently connected socket TCP socket
+//Conn *net.TCPConn
+//The ID of the current connection can also be called SessionID. The ID is globally unique.
+ConnID uint64
+//Message management MsgID and the message management module of the corresponding processing method
+//MsgHandler anet2.MsgHandle
+//Inform the channel that the link has exited/stopped
+ctx context.Context
+cancel context.CancelFunc
+//Unbuffered pipe, used for reading and writing message communication between two goroutines
+msgChanchan[]byte
+//There is a buffer pipe for reading and writing message communication between two goroutines.
+msgBuffChanchan[]byte
 
-	sync.RWMutex
-	//链接属性
-	property map[string]interface{}
-	////保护当前property的锁
-	propertyLock sync.Mutex
-	//当前连接的关闭状态
-	isClosed bool
+sync.RWMutex
+//Link properties
+property map[string]interface{}
+////Protect the lock of the current property
+propertyLock sync.Mutex
+//Closed status of the current connection
+isClosed bool
 
-	fay.Protocol
+fay.Protocol
 }
 
-// NewConnection 创建连接的方法
+// NewConnection method to create a connection
 func NewConnection(fayProxyServer *FayProxyServer, connID uint64, protocol fay.Protocol) fay.Connection {
-	//初始化Conn属性
-	c := &Connection{
-		FayProxyServer: fayProxyServer,
-		ConnID:         connID,
-		isClosed:       false,
-		//MsgHandler:  msgHandler,
-		msgChan:     make(chan []byte),
-		msgBuffChan: make(chan []byte, 1024),
-		property:    nil,
-		Protocol:    protocol,
-	}
-
-	return c
+//Initialize Conn property
+c := &Connection{
+FayProxyServer: fayProxyServer,
+ConnID: connID,
+isClosed: false,
+//MsgHandler: msgHandler,
+msgChan: make(chan []byte),
+msgBuffChan: make(chan []byte, 1024),
+property: nil,
+Protocol: protocol,
 }
 
-// StartWriter 写消息Goroutine， 用户将数据发送给客户端
+return c
+}
+
+// StartWriter writes messages to Goroutine, and the user sends data to the client
 func (c *Connection) StartWriter() {
-	fmt.Println("[Writer Goroutine is running]")
-	defer fmt.Println(c.RemoteAddr().String(), "[conn Writer exit!]")
+fmt.Println("[Writer Goroutine is running]")
+defer fmt.Println(c.RemoteAddr().String(), "[conn Writer exit!]")
 
-	for {
-		select {
-		case data := <-c.msgChan:
-			//有数据要写给客户端
-			if err := c.Write(data); err != nil {
-				fmt.Println("Send Data error:, ", err, " Conn Writer exit")
-				return
-			}
-			//fmt.Printf("Send data succ! data = %+v\n", data)
-		case data, ok := <-c.msgBuffChan:
-			if ok {
-				//有数据要写给客户端
-				if err := c.Write(data); err != nil {
-					fmt.Println("Send Buff Data error:, ", err, " Conn Writer exit")
-					return
-				}
-			} else {
-				fmt.Println("msgBuffChan is Closed")
-				break
-			}
-		case <-c.ctx.Done():
-			return
-		}
-	}
+for {
+select {
+case data := <-c.msgChan:
+//There is data to be written to the client
+if err := c.Write(data); err != nil {
+fmt.Println("Send Data error:, ", err, " Conn Writer exit")
+return
+}
+//fmt.Printf("Send data succ! data = %+v\n", data)
+case data, ok := <-c.msgBuffChan:
+if ok {
+//There is data to be written to the client
+if err := c.Write(data); err != nil {
+fmt.Println("Send Buff Data error:, ", err, " Conn Writer exit")
+return
+}
+} else {
+fmt.Println("msgBuffChan is Closed")
+break
+}
+case <-c.ctx.Done():
+return
+}
+}
 }
 
-// StartReader 读消息Goroutine，用于从客户端中读取数据
+// StartReader reads message Goroutine, used to read data from the client
 func (c *Connection) StartReader() {
-	fmt.Println("[Reader Goroutine is running]")
-	defer fmt.Println(c.RemoteAddr().String(), "[conn Reader exit!]")
-	defer c.Stop()
+fmt.Println("[Reader Goroutine is running]")
+defer fmt.Println(c.RemoteAddr().String(), "[conn Reader exit!]")
+defer c.Stop()
 
-	// 创建拆包解包的对象
-	for {
-		select {
-		case <-c.ctx.Done():
-			return
-		default:
-			r, err := c.GetReader()
-			if err != nil {
-				//logger.ZapLog.Error("GetReader err:", zap.Error(err))
-				return
-			}
-			if _, err := io.ReadAll(r); err != nil {
-				fmt.Println("read msg head error ", err)
-				return
-			}
-		}
-	}
+//Create unpacked and unpacked objects
+for {
+select {
+case <-c.ctx.Done():
+return
+default:
+r, err := c.GetReader()
+if err != nil {
+//logger.ZapLog.Error("GetReader err:", zap.Error(err))
+return
+}
+if _, err := io.ReadAll(r); err != nil {
+fmt.Println("read msg head error ", err)
+return
+}
+}
+}
 }
 
-// Start 启动连接，让当前连接开始工作
+// Start starts the connection and lets the current connection start working
 func (c *Connection) Start() {
-	c.ctx, c.cancel = context.WithCancel(context.Background())
-	//1 开启用户从客户端读取数据流程的Goroutine
-	go c.StartReader()
-	//2 开启用于写回客户端数据流程的Goroutine
-	go c.StartWriter()
-	//按照用户传递进来的创建连接时需要处理的业务，执行钩子方法
-	//c.TCPServer.CallOnConnStart(c)
+c.ctx, c.cancel = context.WithCancel(context.Background())
+//1 Start the Goroutine for the user to read data from the client
+go c.StartReader()
+//2 Enable Goroutine for writing back the client data process
+go c.StartWriter()
+//Execute the hook method according to the business that needs to be processed when creating a connection passed in by the user
+//c.TCPServer.CallOnConnStart(c)
 }
 
-// Stop 停止连接，结束当前连接状态M
+// Stop stops the connection and ends the current connection state M
 func (c *Connection) Stop() {
-	//如果用户注册了该链接的关闭回调业务，那么在此刻应该显示调用
-	//c.TCPServer.CallOnConnStop(c)
+//If the user registers the closing callback service of this link, then the call should be displayed at this moment
+//c.TCPServer.CallOnConnStop(c)
 
-	c.Lock()
-	defer c.Unlock()
+c.Lock()
+defer c.Unlock()
 
-	//如果当前链接已经关闭
-	if c.isClosed == true {
-		return
-	}
+//If the current link is closed
+if c.isClosed == true {
+return
+}
 
-	fmt.Println("Conn Stop()...ConnID = ", c.ConnID)
+fmt.Println("Conn Stop()...ConnID = ", c.ConnID)
 
-	// 关闭socket链接
-	c.ConnClose()
-	//关闭Writer
-	c.cancel()
+// Close the socket link
+c.ConnClose()
+//Close Writer
+c.cancel()
 
-	//将链接从连接管理器中删除
-	c.FayProxyServer.GetConnMgr().Remove(c)
+//Remove the link from the connection manager
+c.FayProxyServer.GetConnMgr().Remove(c)
 
-	//关闭该链接全部管道
-	close(c.msgBuffChan)
-	//设置标志位
-	c.isClosed = true
+//Close all pipes of this link
+close(c.msgBuffChan)
+//Set the flag bit
+c.isClosed = true
 
 }
 
-// GetConnID 获取当前连接ID
+// GetConnID gets the current connection ID
 func (c *Connection) GetConnID() uint64 {
-	return c.ConnID
+return c.ConnID
 }
 
-// SendMsg 直接将Message数据发送数据给远程的TCP客户端
+// SendMsg directly sends Message data to the remote TCP client
 func (c *Connection) SendMsg(msgID uint32, data []byte) error {
-	c.RLock()
-	defer c.RUnlock()
-	if c.isClosed == true {
-		return errors.New("connection closed when send msg")
-	}
-
-	//写回客户端
-	c.msgChan <- data
-
-	return nil
+c.RLock()
+defer c.RUnlock()
+if c.isClosed == true {
+return errors.New("connection closed when send msg")
 }
 
-// SendBuffMsg  发生BuffMsg
+//write back to client
+c.msgChan <- data
+
+return nil
+}
+
+// SendBuffMsg BuffMsg occurs
 func (c *Connection) SendBuffMsg(data []byte) error {
-	c.RLock()
-	defer c.RUnlock()
-	if c.isClosed == true {
-		return errors.New("Connection closed when send buff msg")
-	}
-
-	//写回客户端
-	c.msgBuffChan <- data
-
-	return nil
+c.RLock()
+defer c.RUnlock()
+if c.isClosed == true {
+return errors.New("Connection closed when send buff msg")
 }
 
-// SetProperty 设置链接属性
+//write back to client
+c.msgBuffChan <- data
+
+return nil
+}
+
+// SetProperty sets link properties
 func (c *Connection) SetProperty(key string, value interface{}) {
-	c.propertyLock.Lock()
-	defer c.propertyLock.Unlock()
-	if c.property == nil {
-		c.property = make(map[string]interface{})
-	}
-
-	c.property[key] = value
+c.propertyLock.Lock()
+defer c.propertyLock.Unlock()
+if c.property == nil {
+c.property = make(map[string]interface{})
 }
 
-// GetProperty 获取链接属性
+c.property[key] = value
+}
+
+// GetProperty gets the link properties
 func (c *Connection) GetProperty(key string) (interface{}, error) {
-	c.propertyLock.Lock()
-	defer c.propertyLock.Unlock()
+c.propertyLock.Lock()
+defer c.propertyLock.Unlock()
 
-	if value, ok := c.property[key]; ok {
-		return value, nil
-	}
-
-	return nil, errors.New("no property found")
+if value, ok := c.property[key]; ok {
+return value, nil
 }
 
-// RemoveProperty 移除链接属性
+return nil, errors.New("no property found")
+}
+
+// RemoveProperty removes link properties
 func (c *Connection) RemoveProperty(key string) {
-	c.propertyLock.Lock()
-	defer c.propertyLock.Unlock()
+c.propertyLock.Lock()
+defer c.propertyLock.Unlock()
 
-	delete(c.property, key)
+delete(c.property, key)
 }
 
-// 返回ctx，用于用户自定义的go程获取连接退出状态
+//Return ctx, used for user-defined go process to obtain connection exit status
 func (c *Connection) Context() context.Context {
-	return c.ctx
+return c.ctx
 }
